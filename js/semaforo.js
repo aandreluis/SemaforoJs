2f3fc1f28b10e20b2dfb569b71804723866a7358
aandreluis <andreluisstn@gmail.com>
2021-10-30 12:36:42 -0300

    Feature: Adiciona mudar a velocidade da funcionalidade automatica

--- js/semaforo.js
+++ js/semaforo.js
@@ -1,7 +1,9 @@
 const img = document.getElementById('img'); //chama o elemento img
 const buttons = document.getElementById('buttons'); //chama o elemento pai, a div que contem os botoes
+const range = document.getElementById('range');//chama o elemento input range
 var colorIndex = 0; //variavel para usar como index na array das cores, começa como 0 pois realizará calculos
 var intervalId = null; //pega o id que retorna da função setInterval e coloca na variavel, começa como null
+var intervalValue = 1000; //valor default
 
 //função pega o event (retorno do click nos botoes)
 const trafficLight = (event) => {
@@ -25,7 +27,7 @@ const nextIndex = () => {
 const changeColor = () => { 
     let colors = ['red', 'yellow', 'green'] //array com as 3 cores
     let color = colors[colorIndex];//coloca a cor como um dos elementos da array
-    turnOn[color]();//passa como argumento as cor que está na index no momento
+    turnOn[color]();//passa como argumento a cor que está na index no momento
     nextIndex(); //chama a função para mudar a cor
 }
 
@@ -39,8 +41,40 @@ const turnOn = {
     'red': () => img.src = './img/vermelho.png',//muda o caminho da imagem
     'yellow': () => img.src = './img/amarelo.png',
     'green': () => img.src = './img/verde.png',
-    'automatic': () => intervalId = setInterval(changeColor, 1000) //(ação que irá execurar, tempo de repetição)
+    'automatic': () => intervalId = setInterval(changeColor, intervalValue) //(ação que irá execurar, tempo de repetição)
+}
+
+//função para alterar a velocidade da opção automatica
+function speedAutomatic() {
+    let velocidade = document.getElementById("velocidade");//variavel para mostrar a velocidade do range
+    let rangeValor = parseInt(range.value);//pega o valor do range em inteiro
+    switch (rangeValor) {//de acordo com o valor do range faz ações
+        case    1:
+            intervalValue = 1000;//ação de mudar o valor do intervalo
+            stopAutomatic();//ação de parar o atual setInterval
+            break;
+        case    2:
+            intervalValue = 800;
+            stopAutomatic();
+            break;
+        case    3:
+            intervalValue = 500;
+            stopAutomatic();
+            break;
+        case    4:
+            intervalValue = 300;
+            stopAutomatic();
+            break;
+        case    5:
+            intervalValue = 200;
+            stopAutomatic();
+            break;  
+    }//fim switch 
+    velocidade.innerHTML = rangeValor;//joga o valor da velocidade para o html
+    intervalId = setInterval(changeColor, intervalValue);//pega o novo Id e chama um novo automatico
 }
 
 //adiciona um evento para o click dentro da div pai para todos os botoes
 buttons.addEventListener('click', trafficLight);
+//sempre que ouver um novo input chama a função
+range.addEventListener('input', speedAutomatic);
